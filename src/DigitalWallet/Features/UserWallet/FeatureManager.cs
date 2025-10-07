e8a4f78c9af886202b3ee8af18b168177501487e
hassa <hassa@LAPTOP-U03CN6LR>
2025-10-07 22:08:04 +0330

    fix create wallet endpoint

---
+++ src/DigitalWallet/Features/UserWallet/FeatureManager.cs
@@ -0,0 +1,18 @@
+﻿using DigitalWallet.Features.UserWallet.Common;
+using ServiceCollector.Abstractions;
+
+namespace DigitalWallet.Features.UserWallet;
+
+public abstract class FeatureManager
+{
+    public const string EndpointTagName = "Wallet";
+    public const string Prefix = "/wallets";
+
+    public class ServiceManager : IServiceDiscovery
+    {
+        public void AddServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<WalletService>();
+        }
+    }
+}
