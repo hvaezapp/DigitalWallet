e1dd0b3e9341b40e81061b3371c6ef911c228c29
Hassan Vaezzadeh <hassanvaez2445@gmail.com>
2025-10-12 13:28:15 +0330

    add get wallet transactions endpoint

---
+++ src/DigitalWallet/Features/UserWallet/GetTransactions/Endpoint.cs
@@ -0,0 +1,36 @@
+﻿using Carter;
+using DigitalWallet.Features.UserWallet.Common;
+using DigitalWallet.Infrastructure.Persistence.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalWallet.Features.UserWallet.GetTransactions;
+
+public class Endpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app
+            .MapGroup(FeatureManager.Prefix)
+            .WithTags(FeatureManager.EndpointTagName)
+            .MapGet("/{wallet_id:guid:required}/transactions/",
+            async ([FromRoute(Name = "wallet_id")] Guid Id, WalletDbContextReadOnly _dbContext, CancellationToken cancellationToken) =>
+            {
+                var walletId = WalletId.Create(Id);
+
+                var transactions = await _dbContext.GetTransactions()
+                                            .Where(x => x.WalletId == walletId)
+                                            .OrderByDescending(x => x.CreatedOnUtc)
+                                            .Select(x => new GetTransactionsResponse
+                                            (
+                                                x.CreatedOnUtc,
+                                                x.Description,
+                                                x.Type.ToString(),
+                                                x.Kind.ToString()
+
+                                            )).ToListAsync(cancellationToken);
+
+                return Results.Ok(transactions);
+            });
+    }
+}
---
+++ src/DigitalWallet/Features/UserWallet/GetTransactions/GetTransactionsResponse.cs
@@ -0,0 +1,12 @@
+﻿using DigitalWallet.Features.Transactions.Common;
+
+namespace DigitalWallet.Features.UserWallet.GetTransactions;
+
+public record GetTransactionsResponse
+(
+  DateTime CreatedOn,
+  string Descripiton,
+  string TypeName,
+  string KindName
+);
+
